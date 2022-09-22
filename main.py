5d282ebb3d57800d4347c1c2c0f8c11e9d5e49a4
zihasoo <dlquddbs-@daum.net>
2022-09-22 15:56:52 +0900

    V 2.0
    헤더에 셀레니움으로 안보이도록 수정
    아이디 및 비밀번호를 입력한 것이 콘솔창에 보이지 않도록 수정
    중간 중간 딜레이를 넣어서 리퀘스트 fail이 뜨지 않도록 수정

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from selenium.webdriver.common.by import By
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-from time import sleep
+from time import sleep,time
+import os
 
 
 def time_converter(time: str):
@@ -31,7 +32,10 @@ class Lecture:
 
 class AutomaticAttendance:
     def __init__(self):
-        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+        opt = webdriver.ChromeOptions()
+        opt.add_argument(
+            "user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.79 Safari/537.36")
+        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=opt)
         self.driver.get("https://eclass.seoultech.ac.kr/ilos/main/main_form.acl")
         self.driver.implicitly_wait(time_to_wait=3)
         self.id: str
@@ -43,22 +47,38 @@ class AutomaticAttendance:
     def Login(self):
         self.id = input("ID를 입력하세요: ")
         self.pw = input("비밀번호를 입력하세요: ")
+        print('\n'*200)
 
         # login
         self.driver.find_element(By.CLASS_NAME, "header_login.login-btn-color").click()
-
+        sleep(0.3)
         self.driver.find_element(By.ID, "usr_id").send_keys(self.id)
+        sleep(0.3)
         self.driver.find_element(By.ID, "usr_pwd").send_keys(self.pw)
+        sleep(0.3)
 
         self.driver.find_element(By.CLASS_NAME, "btntype").click()
 
+        os.system('cls' if os.name in ('nt', 'dos') else 'clear')
+
+        try:
+            popup = self.driver.find_elements(By.NAME, "doneclose")  # 팝업창 다시 보지 않기 버튼 찾기
+            for x in popup:
+                x.click()  # 전부 누르기
+        except:
+            pass
+
     def Attendance(self):
         # to do list로 들어가기
+        sleep(1)
         self.driver.find_element(By.XPATH, "//*[@id=\"header\"]/div[4]/div/fieldset/div/div[2]").click()
+        sleep(1)
         # 온라인 강의 클릭
         self.driver.find_element(By.XPATH, "//*[@id=\"todo_pop\"]/div/div[1]/div[2]/div[1]").click()
+        sleep(1)
         # 맨 위의 강의 클릭
         self.driver.find_element(By.CLASS_NAME, "todo_wrap.on").click()
+        sleep(0.5)
         # 시간 측정
         lecture_elements = self.driver.find_elements(By.CLASS_NAME, "lecture-box")
         lectures = []
@@ -81,15 +101,19 @@ class AutomaticAttendance:
             self.driver.find_elements(By.CLASS_NAME, "view")[idx].click()
             sleep(3)
             inner_count = len(cur.inner_lectures)
+
             while True:
                 # 남은 시간이 0보다 클 때만 수강
                 if cur.inner_lectures[cur.inner_idx] > 0:
                     # 영상 재생 iframe 안으로 이동
                     self.driver.switch_to.frame(self.driver.find_element(By.TAG_NAME, "iframe"))
+                    sleep(0.3)
                     # 재생 버튼 클릭
                     self.driver.find_element(By.CLASS_NAME, "vc-front-screen-play-btn").click()
                     # 출석 시간 + 10초 만큼 잠들기
-                    sleep(cur.inner_lectures[cur.inner_idx] + 200)
+                    start = time()
+                    while time() <= start + cur.inner_lectures[cur.inner_idx] + 100:
+                        pass
 
                 # 강의 idx++
                 cur.inner_idx += 1
@@ -98,7 +122,8 @@ class AutomaticAttendance:
                 # iframe 빠져 나오기
                 self.driver.switch_to.default_content()
                 # 다음 강의로 이동 버튼 누르기
-                self.driver.find_element(By.CLASS_NAME, "contents-view-btn.contents-view-btn-right").click()
+                self.driver.find_element(By.ID, "next_").click()
+                sleep(1)
 
             # 내부 강의 개수 만큼 뒤로 가기
             for i in range(inner_count):
@@ -108,8 +133,10 @@ class AutomaticAttendance:
 if __name__ == "__main__":
     a = AutomaticAttendance()
     a.Login()
-    try:
-        while True:
-            a.Attendance()
-    finally:
-        a.EndProcess()
+    while True:
+        a.Attendance()
+        try:
+            while True:
+                a.Attendance()
+        finally:
+            a.EndProcess()
